1aaff2e985be9514cba20fdd240d86588eb0ad28
iamvevaar <69046800+iamvevaar@users.noreply.github.com>
2023-08-04 00:47:28 +0530

    Added two buttons
    This gives more power to the app

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-//LOGIC FOR PC SEARCH
+// Function to generate a random string of letters
+function generateRandomLetters(length) {
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  let randomString = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    randomString += letters.charAt(randomIndex);
+  }
+  return randomString;
+}
+
+// Function to open multiple search tabs
+function openMultipleTabs(topics) {
+  topics.forEach(function (topic) {
+    const searchUrl = `https://www.bing.com/search?q=${encodeURIComponent(
+      topic
+    )}`;
+    window.open(searchUrl, "_blank");
+  });
+}
+
+let numberOfTopics = 20;
+const searchTopics = [];
+
+// Handle radio button change event
+const radioMobile = document.getElementById("radioMobile");
+const radioPc = document.getElementById("radioPc");
+
+radioMobile.addEventListener("change", function () {
+  if (radioMobile.checked) {
+    numberOfTopics = 24;
+  }
+});
+
+radioPc.addEventListener("change", function () {
+  if (radioPc.checked) {
+    numberOfTopics = 30;
+  }
+});
+
 document.getElementById("searchBtnPc").addEventListener("click", function () {
-  // Function to generate a random string of letters
-  function generateRandomLetters(length) {
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    let randomString = "";
-    for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * letters.length);
-      randomString += letters.charAt(randomIndex);
-    }
-    return randomString;
-  }
-
-  // Function to open multiple search tabs
-  function openMultipleTabs(topics) {
-    topics.forEach(function (topic) {
-      const searchUrl = `https://www.bing.com/search?q=${encodeURIComponent(
-        topic
-      )}`;
-      window.open(searchUrl, "_blank");
-    });
-  }
-
-  const numberOfTopics = 20;
-  const searchTopics = [];
+  searchTopics.length = 0; // Clear the previous topics
 
   for (let i = 0; i < numberOfTopics; i++) {
     searchTopics.push(generateRandomLetters(3));
