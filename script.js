abf6a8a35cc15f1377c54bd8658ff3377988082f
iamvevaar <69046800+iamvevaar@users.noreply.github.com>
2023-10-06 00:23:22 +0530

    script migrated with V2.js
    This is the major update to the project changing the whole structure of the app , now from here  we are having some more realistic human seached values , just for experminet , if not work ,then will be rollback to old one

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Function for toggling between the dark and light theme
+// Function for toggling between the dark and light theme
 document.addEventListener("DOMContentLoaded", function () {
   const themeToggle = document.getElementById("themeToggle");
   const body = document.body;
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
 // Update the length of generateRandomLetters based on the slider value
 const lengthSlider = document.getElementById("lengthSlider");
 const lengthDisplay = document.getElementById("lengthDisplay");
@@ -17,8 +16,6 @@ lengthSlider.addEventListener("input", function () {
   const newLength = parseInt(lengthSlider.value);
   lengthDisplay.textContent = newLength;
 });
-
-
 
 // Function to generate a random string of letters
 function generateRandomLetters(length) {
@@ -63,10 +60,51 @@ radioPc.addEventListener("change", function () {
 document.getElementById("searchBtnPc").addEventListener("click", function () {
   searchTopics.length = 0; // Clear the previous topics
   const currentLength = parseInt(lengthSlider.value);
-  for (let i = 0; i < numberOfTopics; i++) {
+  const selectedNumberOfTopics = radioMobile.checked ? 24 : 30;
+  for (let i = 0; i < selectedNumberOfTopics; i++) {
     searchTopics.push(generateRandomLetters(currentLength));
   }
-  // console.log(searchTopics);
 
-  openMultipleTabs(searchTopics);
+  const questionsWithRandomStrings = questions
+    .slice(0, selectedNumberOfTopics)
+    .map((question, index) => question.replace(/x/g, searchTopics[index]));
+
+  // console.log(questionsWithRandomStrings);
+
+  openMultipleTabs(questionsWithRandomStrings);
 });
+
+// List of questions with 'x' to be replaced
+const questions = [
+  "What defines x's length?",
+  "Why care about x?",
+  "Uses of x?",
+  "Meaning of x?",
+  "Calculation of x?",
+  "What is the mean of x?",
+  "Why is this happening x?",
+  "Why do we need this x?",
+  "What does x signify?",
+  "Why is x important?",
+  "How do we calculate x?",
+  "What can x represent?",
+  "Why should we study x?",
+  "What are the implications of x?",
+  "How do we interpret x?",
+  "Why is x relevant?",
+  "What are the properties of x?",
+  "How is x measured?",
+  "Why is x a variable of interest?",
+  "What are the potential values of x?",
+  "How does x impact our understanding?",
+  "Why might x be a key factor?",
+  "What are the different forms of x?",
+  "How can x be applied in practice?",
+  "Why is x a subject of investigation?",
+  "What are the potential effects of x?",
+  "Why is x a topic of discussion?",
+  "What can x reveal about a situation?",
+  "How does x influence outcomes?",
+  "Why is x a variable of concern?",
+  // Add more questions here as needed
+];
